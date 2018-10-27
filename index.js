84de62f8da1c2f7dc1dfe4b1860e467649f060bc
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-10-27 17:22:24 -0600

    liberal cors support on the server

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
+
 const router = require('./routes/resource')
 const app = express()
 
 const port = process.env.PORT || 3000
 
+app.use(cors())
 app.use('/resources', router)
 
 app.use(express.json())
