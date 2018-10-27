ddd5f01b9a4d1e403cde03c834395d1ca5eb5a4e
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-10-27 16:26:49 -0600

    fix type

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const router = require('./routes/resource')
 const app = express()
 
 const port = process.env.PORT || 3000
+
+app.use('/resources', router)
 
 app.use(express.json())
 
