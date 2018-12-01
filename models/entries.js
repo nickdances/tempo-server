32b985be953e986e189bc48da19093c447b19bb6
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-11-30 22:43:56 -0700

    document model/entries with jsdoc

--- models/entries.js
+++ models/entries.js
@@ -1,32 +1,64 @@
+/**
+ * @module models/entries 
+ * */
 const knex = require('../knex')
 
+/** Object representing one user's data for one day
+ *  @typedef {Object} Entry
+    @property {number} id 
+    @property {boolean} flow - User report of menstruation
+    @property {number} day
+    @property {number} month 
+    @property {number} temp - User Basal Body Temperature
+ */
 module.exports = {
-    getAll(uid) {
+    /** getAll  
+     * @param {number} uid - User ID, foreign key in Entries database table
+     * @returns {Array<Entry>}  
+    */
+    getAll: function(uid) {
         return knex('entries')
         .select('id', 'date', 'temp', 'flow')
         .where('user_id', uid)
         .then(entries => transform(entries))
     },
-    create(uid, body) {
+    /** create 
+     * @param {number} uid - User ID, foreign key in Entries database table
+     * @param {Object} body 
+     * @returns {number}  - Created Entry ID
+    */
+    create: function(uid, body) {
         let entry = Object.assign({user_id: uid}, body)
         return knex('entries')
         .insert(entry)
         .returning('id')
     },
-    update(id, body) {
+    /** update 
+     * @param {number} uid - User ID, foreign key in Entries database table
+     * @param {Object} body 
+     * @returns {number} - Updated Entry ID
+    */
+    update: function(id, body) {
         return knex('entries')
         .where('id', id)
         .update(body)
         .returning('id')
     },
-    delete(id) {
+    /** delete 
+     * @param {number} uid - User ID, foreign key in Entries database table
+     * @returns {number} - Deleted Entry ID
+    */
+    delete: function(id) {
         return knex('entries')
         .where('id', id)
         .del()
         .returning('id')
     }
 }
-
+    /** transform forms entries from database objects by extracting day and month and parsing numbers
+     * @param {Array<Object>} entries - raw entries from database table
+     * @returns {Array<Entry>} - entries of the formally defined type
+    */
 function transform(entries) {
     return entries.map(entry => ({
             id:     +entry.id, 
